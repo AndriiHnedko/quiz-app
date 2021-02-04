a5d69e91004ec265370903602e0677c94d6c51c5
Andrii Hnedko <andrii.hnedko@gmail.com>
2021-02-04 15:56:25 +0200

    created home page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Home from './source/screens/Home';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
-  return <></>;
+  return (
+    <SafeAreaProvider>
+      <Home />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
---
+++ source/screens/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StatusBar, StyleSheet, View} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import FieldCategory from '../components/FieldCategory';
+import FieldCategoryList from '../components/FieldCategoryList';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar
+        barStyle="default"
+        backgroundColor={styles.header.backgroundColor}
+      />
+      <View style={styles.container}>
+        <Header />
+        <FieldCategoryList />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(34, 42, 64, 1)',
+    flex: 1,
+  },
+  header: {
+    backgroundColor: 'rgba(45, 66, 91, 1)',
+  },
+});
+
+export default Home;
